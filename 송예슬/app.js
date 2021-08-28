2a1fa330409abe40db9a123005593616b0246062
Songyeseul0409 <chonsa0409@sookmyung.ac.kr>
2021-08-28 17:12:21 +0900

    [송예슬] 수정 : 2-18, 함수 정의 및 표현

--- 송예슬/app.js
+++ 송예슬/app.js
@@ -1,15 +1,51 @@
-// SWITCH
+// FUNCTION DECLARATIONS ---------------------------------
 
-const color = 'red';
-
-switch(color){
-    case 'red' :
-        //action
-        break;
-    case 'yellow' :
-        // action
-        break;
-    default:
-        // action
-        break;    
+function greet (firstName = 'Song', lastName = 'YeSeul'){
+//    console.log('Hello'); // 함수만 호출하면 입력됨
+    return 'Hello' + firstName + ' ' + lastName;
 }
+// return하는 함수는 console.log으로 감싸줘야함.
+// default로 매개변수 지정 가능. 
+
+ console.log(greet())
+
+// FUNCTION EXPRESSIONS -----------------------------------
+
+const square = function (x=3) {
+    return x*x;
+};
+
+console.log(square());
+
+// IMEDIATELY INVOKABLE FUNCTION EXPRESSIONS - IIFEs ------
+// 정의하는 순간 실행되는 함수. 
+
+(function(){
+    console.log('IIFE Ran..');
+})();
+
+(function(name){
+    console.log('Hello ' + name);
+})('Suel');
+
+// PROPERTY METHODS ------------------------------------------
+// object안에 function이 있을 때 : method
+
+// define inside object
+const todo = {
+    add : function(){
+        console.log('Add todo...');
+    },
+    edit : function(id){
+        console.log(`Edit todo ${id}` );
+    }
+}
+
+// define outside.
+todo.delete = function(){
+    console.log('Delete todo...');
+}
+
+todo.add();
+todo.edit(22);
+todo.delete();
