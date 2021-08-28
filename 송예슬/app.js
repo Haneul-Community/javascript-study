0cc73b3fc71a406699d8fe8d121440916dd94758
Songyeseul0409 <chonsa0409@sookmyung.ac.kr>
2021-08-28 16:46:14 +0900

    [송예슬] 수정 : 2-16, If

--- 송예슬/app.js
+++ 송예슬/app.js
@@ -1,27 +1,84 @@
-let val;
+// // Form 
+// if (something){
+//     do something
+// } else {
+//     do something else
+// }
 
-const today = new Date();
+const id = 100;
 
-// Date 설정
-let birthday = new Date('4-9-2000 12:30:00');
-birthday = new Date ('April 9 2000');
-birthday = new Date ('4/9/2000');
+// // EQUAL TO
+// if (id == 100){
+//     console.log('CORRECT');
+// } else {
+//     console.log('INCORRECT');
+// }
 
-// Date 일부만 가져오기
-val = today.getMonth(); // Month는 0부터 시작.
-val = today.getDate();
-val = today.getDay(); // 요일. 0=월?
-val = today.getMinutes();
-val = today.getSeconds();
-val = today.getMilliseconds();
-val = today.getTime();
+// // NOT EQUAL TO
+// if (id != 101){
+//     console.log('CORRECT');
+// } else {
+//     console.log('INCORRECT');
+// }
 
-// 설정한 date 변경
-birthday.setMonth(2);
-birthday.setDate(12);
-birthday.setFullYear(1989);
-birthday.setHours(3);
-birthday.setMinutes(30);
-birthday.setSeconds(10);
+// // Equal to value & type
+// if (id === 100){
+//     console.log('CORRECT');
+// } else {
+//     console.log('INCORRECT');
+// }
 
-console.log(birthday);
+// // Equal to value & type
+// if (id !== 100){
+//     console.log('CORRECT');
+// } else {
+//     console.log('INCORRECT');
+// }
+
+// //Test if undefined : id가 undefined 일때 조건문 안에 넣어야 error 발생 x
+// if (typeof id !== 'undefined'){
+//     console.log(`The ID is ${id}`);
+// } else {
+//     console.log('No ID')
+// }
+
+
+// GREATER Or LESS THAN
+//  if (id >= 100){
+//      console.log(`The ID is ${id}`);
+//  } else {
+//      console.log('No ID')
+//  }
+
+// IF ELSE
+
+const color = 'red';
+
+// if (color === 'red'){
+//     console.log('Color is red');
+// } else if (color === 'blue'){
+//     console.log('Color is blue');
+// } else {
+//     console.log('Color is not red or blue');
+// }
+
+// Logical operations 
+
+const name = 'Song';
+const age = '20';
+
+// And : &&
+if (age >0 && age <12) {
+    console.log(`${name} is a child`);
+} else if (age >=13 && age <= 19){
+    console.log(`${name} is a teenager`);
+} else {
+    console.log(`${name} is an adult`);
+}
+
+// Or : ||
+
+// Ternary operator
+console.log(id === 100 ? 'Correct' : 'Incorrect');
+
+// WITHOUT BRACES : {}없이 들여쓰기로도 가능. 
