fbe50203a101591e1794fa9d95d1da7af458993b
Songyeseul0409 <chonsa0409@sookmyung.ac.kr>
2021-08-29 16:30:30 +0900

    [송예슬] 수정 : 2-21, 지역변수 및 전역변수

--- 송예슬/app.js
+++ 송예슬/app.js
@@ -1,57 +1,30 @@
-// WINDOW METHODS / OBJECTS / PROPERTIES
+// Global Scope
+var a = 1;
+let b = 2;
+const c = 3;
 
-// Alert : 창에 메세지로 뜸
-// alert('Hello world');
-
-// Prompt : 메세지 창으로 입력을 받음. 
-// const input = prompt();
-// alert(input);
-
-// Confirm : 메세지 창으로 확인/취소 응답을 받음
-//  if(confirm('Are you sure')){
-//     console.log('Yes');
-// } else {
-//     console.log('NO');
+// // FUNCTION TEST : function 안에 있는 변수는 거기서만 유효. 
+//  function test() {
+//     var a = 4;
+//     let b = 5;
+//     const c = 6;
+//     console.log('Function Scope: ', a, b, c);
 // }
 
-let val;
+// test();
 
-// Outter height and width
-val = window.outerHeight;
-val = window.outerWidth;
+// IF : var만 global 변수에 영향을 줌. 
+// if(true){
+//     // Block Scope
+//     var a = 4;
+//     let b = 5;
+//     const c = 6;
+//     console.log('If scope: ', a,b,c);
+// }
 
-// Inner height and width
-val = window.innerHeight;
-val = window.innerWidth;
+// FOR : var만 global 변수에 영향을 줌. 
+// for (var a=0; a<10; a++){
+//     console.log(`Loop : ${a}`);
+// }
 
-// Scroll points
-val = window.scrollY;
-val = window.scrollX;
-
-// Location Object
-val = window.location;
-val = window.location.hostname;
-val = window.location.port;
-val = window.location.href;
-val = window.location.search;
-
-//Redirect : 해당 페이지로 이동
-//window.location.href = 'http://google.com';
-// Reload 
-//window.location.reload();
-
-// History Object
-
-// window.history.go(-2);
-// val = window.history.length;
-
-// Navigator Object
-val = window.navigator;
-val = window.navigator.appName;
-val = window.navigator.appVersion;
-val = window.navigator.userAgent;
-val = window.navigator.platform;
-val = window.navigator.vendor;
-val = window.navigator.language;
-
-console.log(val);
+console.log('Global Scope: ', a,b,c);
